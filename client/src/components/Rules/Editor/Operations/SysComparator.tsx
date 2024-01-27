0d967ad04088ba33de55dc155eb85a125af21445
mattkrins <mattkrins@gmail.com>
2024-01-27 16:33:19 +1100

    Improves #12

--- client/src/components/Rules/Editor/Operations/SysComparator.tsx
+++ client/src/components/Rules/Editor/Operations/SysComparator.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "@mantine/form";
 import Conditions from "../Conditions";
-import { Box, Switch, TextInput } from "@mantine/core";
-import { IconBraces } from "@tabler/icons-react";
+import { Box, Grid, Switch, TextInput, Textarea } from "@mantine/core";
+import { IconBraces, IconCheck, IconX } from "@tabler/icons-react";
 import { useEffect } from "react";
 
 export default function SysComparator( { form, index, explorer, actionType }: ActionItem ) {
@@ -17,14 +17,37 @@ export default function SysComparator( { form, index, explorer, actionType }: Ac
         <Switch label="Output result to template"
         mt="xs" {...form.getInputProps(`${actionType}.${index}.output`, { type: 'checkbox' })}
         />
-        {checked&&<TextInput
+        {checked&&
+        <Box>
+            <TextInput
             label="Template Key" withAsterisk mt="xs"
-            description="true/false will be stored in this template key if all conditions evaluate as true."
+            description="Template key will contain output string based on conditions evaluating."
             placeholder="result"
             {...form.getInputProps(`${actionType}.${index}.target`)}
             leftSection={<IconBraces size={16} style={{ display: 'block', opacity: 0.8 }}/>}
             rightSection={explorer('target')}
-        />}
+            />
+            <Grid gutter="xs" align="center" mt="xs" >
+                <Grid.Col span="auto">
+                    <Textarea
+                    placeholder="true"
+                    autosize maxRows={4}
+                    {...form.getInputProps(`${actionType}.${index}.true`)}
+                    leftSection={<IconCheck size={16} style={{ display: 'block', opacity: 0.8 }}/>}
+                    rightSection={explorer('true')}
+                    />
+                </Grid.Col>
+                <Grid.Col span="auto">
+                    <Textarea
+                    placeholder="false"
+                    autosize maxRows={4}
+                    {...form.getInputProps(`${actionType}.${index}.false`)}
+                    leftSection={<IconX size={16} style={{ display: 'block', opacity: 0.8 }}/>}
+                    rightSection={explorer('false')}
+                    />
+                </Grid.Col>
+            </Grid>
+        </Box>}
     </Box>
     )
 }
