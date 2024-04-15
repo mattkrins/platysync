aaea9e48a6cae6b713760d50ff941c80e33afde1
Matt Krins <mattkrins@gmail.com>
2024-04-15 15:43:00 +1000

    moves batch service commands to npm script

--- build/build.js
+++ build/build.js
@@ -7,7 +7,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const json = fs.readFileSync('../package.json', 'utf-8');
 const pack = JSON.parse(json);
 delete pack.devDependencies;
-delete pack.scripts;
+pack.scripts = {
+    "service-install": "WinSW-x64 install service.xml",
+    "service-uninstall": "WinSW-x64 uninstall service.xml",
+    "service-start": "WinSW-x64 start service.xml",
+    "service-stop": "WinSW-x64 stop service.xml",
+};
+
 if (!fs.existsSync('dist')) fs.mkdirSync('dist');
 fs.writeFileSync('dist/package.json', JSON.stringify(pack, undefined, 2), 'utf8');
 if (fs.existsSync('dist/typings')) fs.removeSync('dist/typings');
@@ -26,14 +32,7 @@ if (fs.existsSync('WinSW-x64.exe')){
     <onfailure action="restart" delay="10 sec"/>
 </service>`;
     fs.writeFileSync('dist/service.xml', xml, 'utf8');
-    fs.copyFileSync('WinSW-x64.exe', 'dist/WinSW-x64.exe');    //TODO - move install as service from batch to npm script in prod
-    const service = `@echo off
-echo Installing...
-@echo>start-service.bat
-@echo WinSW-x64 start service.xml> start.bat
-WinSW-x64 install service.xml
-npm install`;
-    fs.writeFileSync('dist/install-service.bat', service, 'utf8');
+    fs.copyFileSync('WinSW-x64.exe', 'dist/WinSW-x64.exe');
 }
 
 console.log(`Build complete: ${__dirname}\\dist\\`);
