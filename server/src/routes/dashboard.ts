c113659e6381fff3fafce3d4466e1f8a8a8955ac
mattkrins <mattkrins@gmail.com>
2024-05-15 22:17:30 +1000

    fix dashboard bug

--- server/src/routes/dashboard.ts
+++ server/src/routes/dashboard.ts
@@ -9,13 +9,15 @@ export default async function (route: FastifyInstance) {
         try {
             const logs1 = await getLogs(history, {
                 from: new Date((new Date()).valueOf() - (24 * 60 * 60 * 1000)),
-                fields: ['message']
+                fields: ['message'],
+                limit: 100
             }) as { message: string }[];
             const evaluatedToday = (logs1||[]).filter(l=>l.message==="Evaluating Rule.").length;
             const executedToday = (logs1||[]).filter(l=>l.message==="Executing Rule.").length;
             const logs2 = await getLogs(history, {
                 from: new Date(2022, 0, 0),
-                fields: ['message']
+                fields: ['message'],
+                limit: 100
             }) as { message: string }[];
             const totalEvaluations = (logs2||[]).filter(l=>l.message==="Evaluating Rule.").length;
             const totalExecutions = (logs2||[]).filter(l=>l.message==="Executing Rule.").length;
